a31199a1388aef832caefd412cd07ee5bfdba976
Shashank Manu Rao <shashank@Shashanks-MacBook-Pro.local>
2026-05-31 01:47:53 +0530

    main function code added

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ai-data-analyst!")
+import asyncio
+from teams.data_analyst import getDataAnalyzerTeam
+from models.groq_model_client import get_model_client
+from utils.docker_util import getDockerCommandLineExecutor,start_docker_container,stop_docker_container
+from autogen_agentchat.messages import TextMessage
 
+async def main():
+    groq_model_client = get_model_client()
+    docker = getDockerCommandLineExecutor()
 
-if __name__ == "__main__":
-    main()
+    team = getDataAnalyzerTeam(docker,groq_model_client)
+
+    try:
+        task = 'Can you give me a graph of types of flowers in my data iris.csv'
+        await start_docker_container()
+
+        async for message in team.run_stream(task=task):
+            print(message)
+    except Exception as e:
+        print(e)
+    finally:
+        await stop_docker_container
+
+if __name__=='__main__':
+    asyncio.run(main())
